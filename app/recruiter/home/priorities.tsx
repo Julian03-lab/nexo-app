1332ede0bab371a195cf03f1d37a0f732e2c3f31
Julian <julian03yt@gmail.com>
2023-11-27 00:21:18 -0300

    Add empty state for trash views and priorities

--- app/recruiter/home/priorities.tsx
+++ app/recruiter/home/priorities.tsx
@@ -2,11 +2,44 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import OfferCard from "../../../components/recruiter/offerCard";
 import useUserStore from "../../../services/context";
+import { NoPrioritiesSVG } from "../../../assets/images/undraw/noPrioritiesSVG";
+import { PressPrioritiesSVG } from "../../../assets/images/undraw/pressPrioritiesSVG";
 
 const priorities = () => {
   const { user } = useUserStore();
 
   const filterData = user.publications.filter((item) => item.priority === true);
+
+  if (filterData.length === 0) {
+    return (
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          flex: 1,
+          paddingHorizontal: 20,
+          paddingVertical: 30,
+          backgroundColor: "#fff",
+          gap: 32,
+        }}
+      >
+        <NoPrioritiesSVG />
+        <Text
+          style={{
+            textAlign: "center",
+            fontFamily: "Roboto_400Regular",
+            color: "#1f2269",
+            fontSize: 16,
+          }}
+        >
+          Actualmente no hay empleos agregados a prioridades. Para hacerlo, haz
+          clic en el botón 'Prioridades' que se encuentra en la barra de
+          navegación de cada empleo que publiques.
+        </Text>
+        <PressPrioritiesSVG />
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
--- app/recruiter/home/trash/semisenior.tsx
+++ app/recruiter/home/trash/semisenior.tsx
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import useUserStore from "../../../../services/context";
 import TrashView from "../../../../components/recruiter/trash/TrashView";
+import { TrashDrawSVG } from "../../../../assets/images/undraw/trashDrawSVG";
 
 const semisenior = () => {
   const { user } = useUserStore();
@@ -9,7 +10,31 @@ const semisenior = () => {
 
   return (
     <View style={{ flex: 1 }}>
-      <TrashView users={userList} />
+      {userList.length > 0 ? (
+        <TrashView users={userList} />
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            paddingHorizontal: 55,
+            gap: 24,
+            backgroundColor: "#fff",
+          }}
+        >
+          <TrashDrawSVG />
+          <Text
+            style={{
+              fontSize: 16,
+              fontFamily: "Roboto_400Regular",
+              color: "#1f2269",
+            }}
+          >
+            Actualmente la papelera esta vacía
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
